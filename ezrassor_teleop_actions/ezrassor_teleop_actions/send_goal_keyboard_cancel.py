ce71d4cfcc58f922cbcbac23018d34f6bc2eccc0
Andy Ponce <andy.ponce@outlook.com>
2022-04-02 23:53:53 -0400

    Consolidated teleop client functions Added better code documentation regarding teleop client

--- ezrassor_teleop_actions/ezrassor_teleop_actions/send_goal_keyboard_cancel.py
+++ ezrassor_teleop_actions/ezrassor_teleop_actions/send_goal_keyboard_cancel.py
@@ -16,9 +16,9 @@ def main(args=None):
   action_client = TeleopActionClient()
 
 # Send an example goal (example is default values of parameters) 
-# Default values of send_goal_with_keyboard: 
-# operation="move-forward", duration=2.5
-  action_client.send_goal_with_keyboard()
+# Default values of send_goal: 
+# cancel_method="", operation="move-forward", duration=2.5, secondsToCancel=1.25
+  action_client.send_goal(cancel_method="keyboard")
   # Spin to execute callbacks
   rclpy.spin(action_client)
 
