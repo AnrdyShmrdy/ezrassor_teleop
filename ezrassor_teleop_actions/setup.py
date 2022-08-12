4076182c5fda21ddab59798e0397f9edd6743be6
Andy Ponce <andy.ponce@outlook.com>
2022-08-12 15:25:09 -0500

    Added ros2 run entrypoints (#29)
    * removed dependency on pynput
    
    * Added ros2 run entrypoints & updated README

--- ezrassor_teleop_actions/setup.py
+++ ezrassor_teleop_actions/setup.py
@@ -15,11 +15,16 @@ setup(
     zip_safe=True,
     maintainer='andyp',
     maintainer_email='andy.ponce@outlook.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='ROS2 version of ezrassor_teleop_actions',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+                    'send_goal_keyboard_cancel = ezrassor_teleop_actions.send_goal_keyboard_cancel',
+                    'send_goal_list = ezrassor_teleop_actions.send_goal_list',
+                    'send_goal_no_cancel = ezrassor_teleop_actions.send_goal_no_cancel',
+                    'send_goal_timer_cancel = ezrassor_teleop_actions.send_goal_timer_cancel',
+                    'teleop_action_server = ezrassor_teleop_actions.teleop_action_server:main'
         ],
     },
 )
